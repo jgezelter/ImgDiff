ef7b96ad1d8433267f1184774de338d063d5cfc6
Jonah Gezelter <gezelter@princeton.edu>
2026-05-21 11:53:55 -0400

    Updating the key_vals function to run faster

--- PBG/square-lattice-legume.py
+++ PBG/square-lattice-legume.py
@@ -17,7 +17,7 @@ import sys
 import os
 
 def key_vals(r):
-    D=0.5
+    D=0.55
     epsr=12
     lattice = legume.Lattice("square")
     phc = legume.PhotCryst(lattice)
@@ -25,36 +25,42 @@ def key_vals(r):
     phc.layers[-1].add_shape(legume.Circle(eps=1.0, r=r))
     gme = legume.GuidedModeExp(phc, gmax=6)
 
-    gme.run(gmode_inds = [0,1,2,3,4,5,6],
-        numeig=40,
-        verbose = True)
-    
-    test_freqs = gme.freqs[0]
-    vals, counts = np.unique(test_freqs.round(7), return_counts=True)
-    test_vals = vals[counts>1]
-
-    test_k = 0.015
+    test_k = 0.03
     num_points = 40
 
     path = lattice.bz_path([[0,0], [test_k,0], [test_k/np.sqrt(2), test_k/np.sqrt(2)], [0,0]], [num_points,1,num_points])
-    band_locs = np.ndarray((len(test_vals), 2, 40, 2))
+    gme.run(
+        kpoints=path["kpoints"], 
+        gmode_inds = [0,1,2,3,4,5,6], 
+        numeig=40,
+        verbose = True
+    )
+
+    tot_freq = gme.freqs
+    test_freqs = tot_freq[0]
+    vals, counts = np.unique(test_freqs.round(7), return_counts=True)
+    test_vals = vals[(counts>1) & (vals != 0)]
+
+    mask = np.isin(test_freqs.round(7), test_vals)
+    masked = tot_freq.transpose()[mask]
+
+    band_locs = np.ndarray((len(test_vals), 2, 2, num_points))
+
     for i in range(len(test_vals)):
-        gme.run(kpoints=path["kpoints"], 
-            gmode_inds = [0,1,2,3,4,5,6],
-            numeig=2, 
-            eig_sigma = test_vals[i],
-            verbose = True)
-        band_locs[i][0]=gme.freqs[0:num_points]
-        band_locs[i][1]=-np.flip(gme.freqs[-1*(num_points+1):-1], 0)
+        band_locs[i][0]=masked[i*2:(i+1)*2, 0:num_points]
+        band_locs[i][1]=-1*np.flip(masked[i*2:(i+1)*2, 1*(num_points+1):-1], 1)
+
     
     aniso = (band_locs.sum(axis=1)**2).sum(axis=(1,2))
 
-    chosen_band =abs(band_locs[aniso == min(aniso)][0])-band_locs[aniso == min(aniso)][0][0][0]
-    dist_to_band = abs(chosen_band).min(axis=(0,2))
+    chosen_band =abs(band_locs[aniso == min(aniso)][0])-band_locs[aniso == min(aniso)][0][0][0][0]
+    dist_to_band = abs(chosen_band).min(axis=(0,1))
     ks = np.array(range(num_points)) * test_k/num_points
 
     a = np.polyfit(ks, dist_to_band, deg=2)[0]
-    return a, np.sqrt(min(aniso))
+    b = np.polyfit(ks, dist_to_band, deg=2)[1]
+    omega = band_locs[aniso == min(aniso)][0][0][0][0]
+    return omega, a,b, np.sqrt(min(aniso))
 
 
 rs = np.linspace(0,0.5, 8000)
@@ -63,15 +69,17 @@ index = int(sys.argv[1])
 
 data = np.ndarray((4,3))
 
-for i in range(4):
-    sub_index = i + 4*index
+for i in range(20):
+    sub_index = i + 20*index
     r = rs[sub_index]
-    a, aniso = key_vals(r)
+    omega, a, b, aniso = key_vals(r)
 
     data[i][0] = r
-    data[i][1] = a
-    data[i][2] = aniso
+    data[i][1] = omega
+    data[i][2] = a
+    data[i][3] = b
+    data[i][4] = aniso
 
-pd.DataFrame(data, columns = ["r", "a", "aniso"]).to_csv(f"results/260520/band_diagram_optimization_2/raw/{index}.csv")
+pd.DataFrame(data, columns = ["r", "a", "aniso", "b"]).to_csv(f"results/260521/band_diagram_optimization/raw/{index}.csv")
     
     
--- batch_processing/batch-submission.sh
+++ batch_processing/batch-submission.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=square-lattice-bd
 #SBATCH --time=1:30:00
-#SBATCH --output=results/260520/band_diagram_optimization_2/out/batch.%a.out
+#SBATCH --output=results/260521/band_diagram_optimization/out/batch.%a.out
 #SBATCH --mem=2G
 #SBATCH --ntasks-per-node=1
 #SBATCH --constraint=genoa
-#SBATCH --array=0-1999
+#SBATCH --array=0-399
 
 echo "running on node: $SLURMD_NODENAME"
 
